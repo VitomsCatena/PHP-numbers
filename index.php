07a3fe131142f508a10290d6527561296a4cd981
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-08-23 08:53:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
     <title></title>
   </head>
   <body>
+<!--php-->
+<?php
+// Write your code below:
+$my_num = 21;
+$answer = $my_num;
+  $answer += 2;
+ $answer *=2 ; 
+  $answer -= 2;
+  $answer /= 2 ;
+  $answer -= $my_num;
+  echo $answer;
+?>
     <div class="container">
     </div>
   </body>
